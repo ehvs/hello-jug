f4e1645326b799f928e4bf1f9f6f779e72c9b129
Hevellyn Gomes <hgomes@localhost.localdomain>
2018-09-18 09:33:01 -0300

    files

---
+++ src/main/java/com/hevs/demo/hevsdemospringboot/HevsDemoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.hevs.demo.hevsdemospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HevsDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HevsDemoSpringBootApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hevs/demo/hevsdemospringboot/resource/HelloResource.java
@@ -0,0 +1,15 @@
+package com.hevs.demo.hevsdemospringboot.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloResource {
+
+    @GetMapping
+    public String hello() {
+        return "Hello JUG!";
+    }
+}
